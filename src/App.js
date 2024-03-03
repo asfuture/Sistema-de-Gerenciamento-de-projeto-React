5dfa0830699352ad1c926755b409bb48df1d5f94
Alex Ferreira <asfuture2019@gmail.com>
2024-03-03 15:53:07 -0300

    inserindo CSS no React

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import './App.css';
 import HelloWorld from './components/HelloWorld';
 import SayMayName from './components/sayMyName';
 import Pessoa from './components/Pessoa';
+import Frase from './components/Frase';
 
 function App() {
   const nome = "Luana"
   return (
     <div className="App">
+      <h1>Testando h1</h1>
+      <Frase />
+      <Frase />
         <SayMayName  nome="Alex Ferreira"/>
         <SayMayName  nome="João"/>
         <SayMayName  nome={nome}/>
